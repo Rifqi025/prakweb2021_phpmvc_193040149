2bed7230685ca64615ff145b46037590ccb7cf4f
Rifqi025 <muliawan027@gmail.com>
2021-10-01 23:28:21 +0700

    membuat file mahasiswa.php,  membuat data base yang berisi mahasiswa dan menyelesaikan video ke 7 - Model

--- app/views/about/index.php
+++ app/views/about/index.php
@@ -2,7 +2,7 @@
 
     <h1 class="mt-4">About Me</h1>
     <img src="<?= BASEURL; ?>/img/profile.jpeg" alt="Rifqi Muliawan"
-    width="150" height="150" class="rounded-circle">
+    width="300" height="300" class="rounded-circle">
     <p>Halo, nama saya <?= $data['nama']; ?>, umur saya <?= $data['umur']; ?> tahun,
     saya adalah seorang <?= $data['pekerjaan']; ?>.</p>
 </div>
